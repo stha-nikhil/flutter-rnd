f7d72446d8e215fb4a443193544558be13511cb9
stha-nikhil <shrestha.nikhil01@gmail.com>
2023-03-14 19:50:33 +0545

    FEAT: implementation of scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,15 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Demo App'),
+        ),
+        body: Text('This is body.'),
+      ),
     );
   }
 }

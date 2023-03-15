0284fd51c748b90bccd930956fe750697c9bd4e1
stha-nikhil <shrestha.nikhil01@gmail.com>
2023-03-15 14:57:41 +0545

    FEAT: add stateless widget with elevated button and text

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,33 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answer() {
+    print('Answer Recorded!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var question = 'What does HTML stand for?';
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Demo App'),
         ),
-        body: Text('This is body.'),
+        body: Column(
+          children: [
+            Text(question),
+            Text('The question: '),
+            ElevatedButton(
+              onPressed: answer,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: answer,
+              child: Text('Answer 2'),
+            )
+          ],
+        ),
       ),
     );
   }

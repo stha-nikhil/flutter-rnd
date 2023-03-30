e9f7dc9066392519deafbcc92d2a714f6eb29e2a
stha-nikhil <shrestha.nikhil01@gmail.com>
2023-03-30 20:50:51 +0545

    FEAT: add text controller for TextFormField

--- lib/feedback.dart
+++ lib/feedback.dart
@@ -10,6 +10,10 @@ class FeedBackForm extends StatefulWidget {
 class _FeedBackFormState extends State<FeedBackForm> {
   final _formKey = GlobalKey<FormState>();
 
+  final TextEditingController _nameTextController = TextEditingController();
+  final TextEditingController _addressTextController = TextEditingController();
+  final TextEditingController _messageTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -17,13 +21,29 @@ class _FeedBackFormState extends State<FeedBackForm> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          SizedBox(
+            height: 30,
+          ),
+          const Text(
+            'Feedback Form',
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.blueGrey,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
           TextFormField(
+            controller: _nameTextController,
             decoration: const InputDecoration(
                 labelText: 'Name',
                 hintText: 'Please enter your name',
                 icon: Icon(Icons.person)),
           ),
           TextFormField(
+            controller: _addressTextController,
             decoration: const InputDecoration(
                 labelText: 'Contact',
                 hintText: 'Please enter your email / phone number',
@@ -31,18 +51,33 @@ class _FeedBackFormState extends State<FeedBackForm> {
           ),
           Card(
             color: Colors.grey.shade100,
-            child: const Padding(
-              // padding: EdgeInsets.only(top: 12.0),
-              padding: EdgeInsets.symmetric(horizontal: 0.5, vertical: 15),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 0.1, vertical: 15),
               child: TextField(
                 maxLines: 6,
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
+                controller: _messageTextController,
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Feedback',
                   hintText: 'Please enter your name',
                   icon: Icon(Icons.inbox),
                 ),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 150,
+            height: 60,
+            child: ElevatedButton(
+              onPressed: (){
+                _nameTextController.clear();
+                _messageTextController.clear();
+                _addressTextController.clear();
+                },
+              child: const Text(
+                'Submit',
+                style: TextStyle(color: Colors.white),
               ),
             ),
           )

0117b6733e374f428704a5fb1ad97d8b610f1a28
stha-nikhil <shrestha.nikhil01@gmail.com>
2023-04-04 14:44:15 +0545

    FEAT: add email TextFormField with regex validation

--- lib/feedback.dart
+++ lib/feedback.dart
@@ -11,12 +11,13 @@ class _FeedBackFormState extends State<FeedBackForm> {
   final _formKey = GlobalKey<FormState>();
 
   final TextEditingController _nameTextController = TextEditingController();
-  final TextEditingController _addressTextController = TextEditingController();
+  final TextEditingController _phoneTextController = TextEditingController();
+  final TextEditingController _emailTextController = TextEditingController();
   final TextEditingController _messageTextController = TextEditingController();
 
   bool _validate = false;
 
-  void _submit(){
+  void _submit() {
     setState(() {
       _validate = true;
     });
@@ -33,100 +34,124 @@ class _FeedBackFormState extends State<FeedBackForm> {
     return null;
   }
 
-
   @override
-Widget build(BuildContext context) {
-  return Form(
-    key: _formKey,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        SizedBox(
-          height: 30,
-        ),
-        const Text(
-          'Feedback Form',
-          style: TextStyle(
-            fontSize: 25,
-            color: Colors.blueGrey,
-            fontWeight: FontWeight.bold,
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          const SizedBox(
+            height: 25,
           ),
-        ),
-        SizedBox(
-          height: 50,
-        ),
-        TextFormField(
-          controller: _nameTextController,
-          decoration: const InputDecoration(
-              labelText: 'Name',
-              hintText: 'Please enter your name',
-              icon: Icon(Icons.person)),
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return '*Required Name';
-            }
-            return null;
-          },
-        ),
-        TextFormField(
-          controller: _addressTextController,
-          decoration: const InputDecoration(
-              labelText: 'Contact',
-              hintText: 'Please enter your phone number',
-              icon: Icon(Icons.contact_page)),
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return '*Required Contact';
-            }
-            final check = num.tryParse(value);
-            if (check == null) {
-              return '*Only numbers allowed';
-            }
-            return null;
-          },
-        ),
-        Card(
-          color: Colors.grey.shade100,
-          child: Padding(
-            padding:
-            const EdgeInsets.symmetric(horizontal: 0.1, vertical: 15),
-            child: TextField(
-              maxLines: 6,
-              controller: _messageTextController,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                labelText: 'Feedback',
-                hintText: 'Please enter your feedback',
-                icon: const Icon(Icons.inbox),
-                errorText: _validate ? _messageErrorText : null,
+          const Text(
+            'Feedback Form',
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.blueGrey,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          TextFormField(
+            controller: _nameTextController,
+            decoration: const InputDecoration(
+                labelText: 'Name',
+                hintText: 'Please enter your name',
+                icon: Icon(Icons.person)),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return '*Required Name';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _phoneTextController,
+            decoration: const InputDecoration(
+                labelText: 'Phone Number',
+                hintText: 'Please enter your phone number',
+                icon: Icon(Icons.contact_page)),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return '*Required Phone Number';
+              }
+              if (value.length != 10) {
+                return '*Invalid Phone Number';
+              }
+              final check = num.tryParse(value);
+              if (check == null) {
+                return '*Only numbers allowed';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _emailTextController,
+            decoration: const InputDecoration(
+                labelText: 'Email',
+                hintText: 'Please enter your email address',
+                icon: Icon(Icons.email)),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return '*Required Email';
+              }
+              bool emailValid = RegExp(
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                  .hasMatch(value);
+              if (emailValid == false) {
+                return '*Invalid email';
+              }
+              return null;
+            },
+          ),
+          Card(
+            color: Colors.grey.shade100,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 0.1, vertical: 15),
+              child: TextField(
+                maxLines: 6,
+                controller: _messageTextController,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  labelText: 'Feedback',
+                  hintText: 'Please enter your feedback',
+                  icon: const Icon(Icons.inbox),
+                  errorText: _validate ? _messageErrorText : null,
+                ),
               ),
             ),
           ),
-        ),
-        SizedBox(
-          width: 150,
-          height: 60,
-          child: ElevatedButton(
-            onPressed: () {
-              _submit();
-              if (_formKey.currentState!.validate() &&
-                  _messageTextController.text.isNotEmpty) {
-                _nameTextController.clear();
-                _messageTextController.clear();
-                _addressTextController.clear();
-                _messageTextController.clear();
-                _validate = false;
-                ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Validation successful')));
-              }
-            },
-            child: const Text(
-              'Submit',
-              style: TextStyle(color: Colors.white),
+          SizedBox(
+            width: 150,
+            height: 70,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: ElevatedButton(
+              onPressed: () {
+                _submit();
+                if (_formKey.currentState!.validate() &&
+                    _messageTextController.text.isNotEmpty) {
+                  _nameTextController.clear();
+                  _messageTextController.clear();
+                  _phoneTextController.clear();
+                  _emailTextController.clear();
+                  _validate = false;
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Validation successful')));
+                }
+              },
+              child: const Text(
+                'Submit',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
+          )
           ),
-        )
-      ],
-    ),
-  );
-}}
+        ],
+      ),
+    );
+  }
+}

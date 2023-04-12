6c31a613c840e6a1baf3c9009c651142adbf46d8
stha-nikhil <shrestha.nikhil01@gmail.com>
2023-04-12 13:48:33 +0545

    FEAT: add go_router package and implement in App page

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import 'drawer.dart';
 import 'feedback.dart';
 import 'quiz.dart';
 import 'register.dart';
-import 'src/app.dart';
 import 'style.dart';
 
 void main() => runApp(MyApp());
+
+final GoRouter _router = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+          path: '/',
+          builder: (BuildContext context, GoRouterState state) {
+            return const AppDrawer(RegisterForm());
+          },
+          routes: <RouteBase>[
+            GoRoute(
+                path: 'quiz',
+                builder: (BuildContext context, GoRouterState state) {
+                  return const AppDrawer(Quiz());
+                }
+            ),
+            GoRoute(
+              path: 'feedback',
+              builder: (BuildContext context, GoRouterState state){
+                return const AppDrawer(FeedBackForm());
+              }
+            )
+          ]
+      )
+    ]
+);
 
 class MyApp extends StatefulWidget {
   @override
@@ -17,8 +42,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       // home: const AppDrawer(RegisterForm()),
+      routerConfig: _router,
       theme: ThemeData(
         primarySwatch: Colors.teal,
         scaffoldBackgroundColor: Colors.grey.shade100,
@@ -36,12 +62,12 @@ class _MyAppState extends State<MyApp> {
         drawerTheme: drawerStyle,
         cardTheme: cardStyle,
       ),
-      initialRoute: '/',
-      routes: {
-        '/':(context) => const AppDrawer(RegisterForm()),
-        '/quiz': (context) => const AppDrawer(Quiz()),
-        '/feedback': (context) => const AppDrawer(FeedBackForm()),
-      },
+      // initialRoute: '/',
+      // routes: {
+      //   '/': (context) => const AppDrawer(RegisterForm()),
+      //   '/quiz': (context) => const AppDrawer(Quiz()),
+      //   '/feedback': (context) => const AppDrawer(FeedBackForm()),
+      // },
     );
   }
 }

4acdb08a45ff082e8093da950b11c86629f7e1b8
stha-nikhil <shrestha.nikhil01@gmail.com>
2023-04-18 20:34:49 +0545

    FEAT: add change notifier provider in consumer

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -15,64 +15,74 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<CountPoints>(builder: (context, questionProvider, child) {
-      return questionProvider.questionIndex < questionProvider.questions.length
-          ? Column(
-              children: [
-                const SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: CardTitle(null),
-                ),
-                const SizedBox(
-                  height: 70,
-                ),
-                // Question(
-                //   questionProvider.questions[questionProvider.questionIndex]
-                //   ['questionText'] as String,
-                // ),
-                Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.all(9.8),
-                  child: Text(
-                    questionProvider.questions[questionProvider.questionIndex]
-                        ['questionText'] as String,
-                    style: Theme.of(context).textTheme.titleMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                const SizedBox(
-                  height: 80,
-                ),
-                ...(questionProvider.questions[questionProvider.questionIndex]
-                        ['answers'] as List<Map<String, Object>>)
-                    .map((answer) {
-                  return Container(
-                    margin: const EdgeInsets.all(10),
-                    child: Container(
-                      padding: const EdgeInsets.all(5),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          questionProvider.answerFunc(answer['points'] as int);
-                        },
-                        child: Text(
-                          answer['answerText'] as String,
-                          style: Theme.of(context).textTheme.labelMedium,
-                        ),
+    return ChangeNotifierProvider<CountPoints>(
+      create: (context) => CountPoints(),
+      child: Consumer<CountPoints>(
+        builder: (context, questionProvider, child) {
+          return questionProvider.questionIndex <
+                  questionProvider.questions.length
+              ? Column(
+                  children: [
+                    const SizedBox(
+                      width: 200,
+                      height: 50,
+                      child: CardTitle(null),
+                    ),
+                    const SizedBox(
+                      height: 70,
+                    ),
+                    // Question(
+                    //   questionProvider.questions[questionProvider.questionIndex]
+                    //   ['questionText'] as String,
+                    // ),
+                    Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.all(9.8),
+                      child: Text(
+                        questionProvider
+                                .questions[questionProvider.questionIndex]
+                            ['questionText'] as String,
+                        style: Theme.of(context).textTheme.titleMedium,
+                        textAlign: TextAlign.center,
                       ),
                     ),
-                    // Answer(
-                    //     () => questionProvider
-                    //         .answerFunc(answer['points'] as int),
-                    //     answer['answerText'] as String),
-                  );
-                }).toList(),
-              ],
-            )
-          : Result(questionProvider.totalPoint, questionProvider.reset);
-    });
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    ...(questionProvider
+                                .questions[questionProvider.questionIndex]
+                            ['answers'] as List<Map<String, Object>>)
+                        .map((answer) {
+                      return Container(
+                          margin: const EdgeInsets.all(10),
+                          child: Container(
+                            padding: const EdgeInsets.all(5),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                questionProvider
+                                    .answerFunc(answer['points'] as int);
+                              },
+                              child: Text(
+                                answer['answerText'] as String,
+                                style: Theme.of(context).textTheme.labelMedium,
+                              ),
+                            ),
+                          ),
+                          // Answer(
+                          //     () => questionProvider
+                          //         .answerFunc(answer['points'] as int),
+                          //     answer['answerText'] as String),
+                          );
+                    }).toList(),
+                  ],
+                )
+              : Result(questionProvider.totalPoint, questionProvider.reset);
+        },
+      ),
+    );
   }
 }
 //

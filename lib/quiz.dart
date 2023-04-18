b46f378fa2eb0735ad0fbc6f6b7a304663371b39
stha-nikhil <shrestha.nikhil01@gmail.com>
2023-04-18 20:33:22 +0545

    FEAT: add consumer in quiz

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/question.dart';
+import 'package:provider/provider.dart';
 
 import 'answer.dart';
 import 'drawer.dart';
-import 'feedback.dart';
 import 'points.dart';
+import 'src/providers/count_provider.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({Key? key}) : super(key: key);
@@ -14,89 +15,117 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  var _questionIndex = 0;
-  var _totalPoint = 0;
-
-  static const questions = [
-    {
-      'questionText': 'A compiler transforms _____',
-      'answers': [
-        {'answerText': 'Code', 'points': 1},
-        {'answerText': 'Swift', 'points': 0},
-        {'answerText': 'Braces', 'points': 0}
-      ]
-    },
-    {
-      'questionText': 'Which of these is a programming language?',
-      'answers': [
-        {'answerText': 'HTML', 'points': 0},
-        {'answerText': 'Python', 'points': 1}
-      ]
-    }
-  ];
-
-  void _answer(int points) {
-    _totalPoint += points;
-    setState(() {
-      _questionIndex += 1;
-    });
-  }
-
-  void _reset() {
-    setState(() {
-      _totalPoint = 0;
-      _questionIndex = 0;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return QuizAnswer(questions, _questionIndex, _answer, _totalPoint, _reset);
+    return Consumer<CountPoints>(builder: (context, questionProvider, child) {
+      return questionProvider.questionIndex < questionProvider.questions.length
+          ? Column(
+              children: [
+                const SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: CardTitle(null),
+                ),
+                const SizedBox(
+                  height: 70,
+                ),
+                // Question(
+                //   questionProvider.questions[questionProvider.questionIndex]
+                //   ['questionText'] as String,
+                // ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.all(9.8),
+                  child: Text(
+                    questionProvider.questions[questionProvider.questionIndex]
+                        ['questionText'] as String,
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(
+                  height: 80,
+                ),
+                ...(questionProvider.questions[questionProvider.questionIndex]
+                        ['answers'] as List<Map<String, Object>>)
+                    .map((answer) {
+                  return Container(
+                    margin: const EdgeInsets.all(10),
+                    child: Container(
+                      padding: const EdgeInsets.all(5),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          questionProvider.answerFunc(answer['points'] as int);
+                        },
+                        child: Text(
+                          answer['answerText'] as String,
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                      ),
+                    ),
+                    // Answer(
+                    //     () => questionProvider
+                    //         .answerFunc(answer['points'] as int),
+                    //     answer['answerText'] as String),
+                  );
+                }).toList(),
+              ],
+            )
+          : Result(questionProvider.totalPoint, questionProvider.reset);
+    });
   }
 }
-
-class QuizAnswer extends StatelessWidget {
-  const QuizAnswer(this.questions, this.questionIndex, this.answerFunc,
-      this.points, this.reset,
-      {super.key});
-
-  final List<Map<String, Object>> questions;
-  final int questionIndex;
-  final Function answerFunc;
-  final int points;
-  final VoidCallback reset;
-
-  @override
-  Widget build(BuildContext context) {
-    return questionIndex < questions.length
-        ? Column(
-            children: [
-              const SizedBox(
-                width: 200,
-                height: 50,
-                child: CardTitle(null),
-              ),
-              const SizedBox(
-                height: 70,
-              ),
-              Question(
-                questions[questionIndex]['questionText'] as String,
-              ),
-              const SizedBox(
-                height: 80,
-              ),
-              ...(questions[questionIndex]['answers']
-                      as List<Map<String, Object>>)
-                  .map((answer) {
-                return Container(
-                  margin: const EdgeInsets.all(10),
-                  child: Answer(() => answerFunc(answer['points']),
-                      answer['answerText'] as String),
-                );
-              }).toList(),
-            ],
-          )
-        :
-              Result(points, reset);
-  }
-}
+//
+// class MyQuiz extends StatelessWidget {
+//   const MyQuiz({super.key});
+//
+//   // const QuizAnswer(this.questions, this.questionIndex, this.answerFunc,
+//   //     this.points, this.reset,
+//   //     {super.key});
+//   //
+//   // final List<Map<String, Object>> questions;
+//   // final int questionIndex;
+//   // final Function answerFunc;
+//   // final int points;
+//   // final VoidCallback reset;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Consumer<CountPoints>(
+//       builder: ((context, questionProvider, child) =>
+//           questionProvider.questionIndex < questionProvider.questions.length
+//               ? Column(
+//                   children: [
+//                     const SizedBox(
+//                       width: 200,
+//                       height: 50,
+//                       child: CardTitle(null),
+//                     ),
+//                     const SizedBox(
+//                       height: 70,
+//                     ),
+//                     Question(
+//                       questionProvider.questions[questionProvider.questionIndex]
+//                           ['questionText'] as String,
+//                     ),
+//                     const SizedBox(
+//                       height: 80,
+//                     ),
+//                     ...(questionProvider
+//                                 .questions[questionProvider.questionIndex]
+//                             ['answers'] as List<Map<String, Object>>)
+//                         .map((answer) {
+//                       return Container(
+//                         margin: const EdgeInsets.all(10),
+//                         child: Answer(
+//                             () => questionProvider
+//                                 .answerFunc(answer['points'] as int),
+//                             answer['answerText'] as String),
+//                       );
+//                     }).toList(),
+//                   ],
+//                 )
+//               : Result(questionProvider.totalPoint, questionProvider.reset)),
+//     );
+//   }
+// }
